c3274c401c2042fce3707b6b9e70f8ef4c0de4e2
Tomas Basham <tomasbasham@gmail.com>
2022-08-09 08:01:06 +0000

    Rename docs to guides
    For consistency with the type of content being consumed from this
    portal, this commit changes the base path for documentation from `docs`
    to `guides`.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,11 +33,10 @@ const config = {
       ({
         blog: false,
         docs: {
+          path: 'guides',
+          routeBasePath: 'guides',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/StatusCakeDev/portal/tree/master/',
         },
         googleAnalytics: {
           trackingID: 'UA-32558906-2',
