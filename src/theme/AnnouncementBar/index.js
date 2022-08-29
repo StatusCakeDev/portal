2ef22fe33a9f7d4507ed33090d9c8f39d1f733bf
Tomas Basham <tomasbasham@gmail.com>
2022-08-29 22:25:50 +0000

    Increase the size of the AnnouncementBar component
    The default height of the announcement bar is too smaller. This commit
    increases the height of the component. Unfortunately the variable that
    controls the height is rooted to the component itself instead of being
    global. Therefore the component needs to be "swizzled" and modified.

---
+++ src/theme/AnnouncementBar/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import {useAnnouncementBar} from '@docusaurus/theme-common/internal';
+import {translate} from '@docusaurus/Translate';
+import IconClose from '@theme/Icon/Close';
+import styles from './styles.module.css';
+export default function AnnouncementBar() {
+  const {isActive, close} = useAnnouncementBar();
+  const {announcementBar} = useThemeConfig();
+  if (!isActive) {
+    return null;
+  }
+  const {content, isCloseable} = announcementBar;
+  return (
+    <div
+      className={styles.announcementBar}
+      role="banner">
+      {isCloseable && <div className={styles.announcementBarPlaceholder} />}
+      <div
+        className={styles.announcementBarContent}
+        // Developer provided the HTML, so assume it's safe.
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{__html: content}}
+      />
+      {isCloseable ? (
+        <button
+          type="button"
+          className={clsx('clean-btn close', styles.announcementBarClose)}
+          onClick={close}
+          aria-label={translate({
+            id: 'theme.AnnouncementBar.closeButtonAriaLabel',
+            message: 'Close',
+            description: 'The ARIA label for close button of announcement bar',
+          })}>
+          <IconClose width={14} height={14} strokeWidth={3.1} />
+        </button>
+      ) : null}
+    </div>
+  );
+}
